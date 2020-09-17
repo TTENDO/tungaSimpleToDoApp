d0bf525f932fdb270b92e7a9fb1bc3c9248b3429
TTENDO <ttendobecky@gmail.com>
2020-09-17 16:40:02 +0300

    added delete Todo

--- script.js
+++ script.js
@@ -1,24 +1,51 @@
 //variables
 const inputEl = document.querySelector(".inputEl");
+const addButton = document.querySelector(".addToDoBtn");
 const todos = document.querySelector(".todos");
 const todolist = [];
 
+//Delete Todo
+function deleteTodo() {
+  const delButtons = document.querySelectorAll(".delBtn");
+  delButtons.forEach(function (button, index) {
+    console.log(button);
+    button.onclick = function () {
+      console.log("clicked");
+      console.log(index);
+      todolist.splice(index, 1);
+      this.parentElement.style.display = "none";
+      console.log(todolist);
+    };
+  });
+}
 //create Todo
 
 function createTodo() {
   todolist.push(inputEl.value);
   const singleTodo = document.createElement("p");
+  const delbtn = document.createElement("button");
+  delbtn.innerHTML = "x";
+  delbtn.className = "btn btn-danger delBtn";
 
-  todolist.forEach(function (todoitem) {
+  todolist.forEach(function (todoitem, itemid) {
     singleTodo.innerText = todoitem;
-    console.log(todolist);
+    singleTodo.appendChild(delbtn);
+    delbtn.className = `btn btn-danger delBtn ${itemid}`;
   });
   todos.appendChild(singleTodo); //inject each todo within the div of class todos
+
+  deleteTodo();
 }
-inputEl.onchange = createTodo;
+
+//inputEl.onchange = createTodo;
+
+addButton.onclick = createTodo;
 
 //?CRUD - Create Read Update Delete
 
-//TODO delete Todo
-//TODO update Todo
-//TODO design.
+// // TODO Delete Todo .
+//TODO Update Todo .
+//TODO Design .
+
+//TODO Create persistant todos via localstorage
+//TODO
